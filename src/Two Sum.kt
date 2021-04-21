7eb147a99900370eb3ffd8a60ae6736d2b9c009a
akshaykale <ark.akshaykale@gmail.com>
2021-04-22 08:30:10 +0900

    optimized two sum

--- src/Two Sum.kt
+++ src/Two Sum.kt
@@ -4,16 +4,20 @@ class Solution {
         val map = hashMapOf<Int, Int>()
 
         for(i in nums.indices) {
+            val compliment = target - nums[i]
+            map[compliment]?.let {
+                return intArrayOf(it, i)
+            }
             map[nums[i]] = i
         }
 
-        for (i in nums.indices) {
-            val a = nums[i]
-            val b = target - a
-            if(map[b] != null) {
-                return intArrayOf(i, map[b]!!)
-            } else continue
-        }
+        // for (i in nums.indices) {
+        //     val a = nums[i]
+        //     val b = target - a
+        //     if(map[b] != null) {
+        //         return intArrayOf(i, map[b]!!)
+        //     } else continue
+        // }
         return intArrayOf(0,0) //Nothing found
     }
 }
