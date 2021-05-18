2b4ce171c666247315718f468bb08eb7ecb5d3b3
Akshay Kale <ark.akshaykale@gmail.com>
2021-05-18 18:06:31 +0900

    110. Balanced Binary Tree

---
+++ src/110. Balanced Binary Tree.kt
@@ -0,0 +1,57 @@
+/**
+https://leetcode.com/problems/balanced-binary-tree/
+
+Given a binary tree, determine if it is height-balanced.
+
+For this problem, a height-balanced binary tree is defined as:
+
+a binary tree in which the left and right subtrees of every node differ in height by no more than 1.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+Example 2:
+
+
+Input: root = [1,2,2,3,3,null,null,4,4]
+Output: false
+Example 3:
+
+Input: root = []
+Output: true
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-104 <= Node.val <= 104
+*/
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun isBalanced(root: TreeNode?): Boolean {
+        if (root == null) return true
+        else if (Math.abs(height(root?.left) - height(root?.right)) > 1) return false
+        return isBalanced(root?.left) && isBalanced(root?.right)
+        
+        
+    }
+    
+    fun height(root: TreeNode?): Int {
+        if (root == null) return 0
+        return 1 + Math.max(height(root.left), height(root.right))
+    }
+}
