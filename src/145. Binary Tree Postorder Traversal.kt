05084c189634b1b01cdee4d57143e97f08a96881
Akshay Kale <ark.akshaykale@gmail.com>
2021-05-19 16:57:39 +0900

    145. Binary Tree Postorder Traversal

---
+++ src/145. Binary Tree Postorder Traversal.kt
@@ -0,0 +1,67 @@
+/**
+https://leetcode.com/problems/binary-tree-postorder-traversal/
+
+
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [3,2,1]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+
+Input: root = [1,2]
+Output: [2,1]
+Example 5:
+
+
+Input: root = [1,null,2]
+Output: [2,1]
+ 
+
+Constraints:
+
+The number of the nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+*/
+
+
+
+
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun postorderTraversal(root: TreeNode?): List<Int> {
+        val list = mutableListOf<Int>()
+        
+        root?.postOrder(list)
+        
+        return list
+    }
+    
+    fun TreeNode.postOrder(list: MutableList<Int>) {
+        this.left?.postOrder(list)
+        this.right?.postOrder(list)
+        list.add(this.`val`)
+    }
+}
