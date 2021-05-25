63ddef2d7e063c519b7659ae905214e7446e6ec2
Akshay Kale <ark.akshaykale@gmail.com>
2021-05-25 10:33:47 +0900

    1249. Minimum Remove to Make Valid Parentheses

---
+++ src/medium/1249. Minimum Remove to Make Valid Parentheses.kt
@@ -0,0 +1,79 @@
+/**
+https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
+
+Given a string s of '(' , ')' and lowercase English characters. 
+
+Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+Formally, a parentheses string is valid if and only if:
+
+It is the empty string, contains only lowercase characters, or
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+ 
+
+Example 1:
+
+Input: s = "lee(t(c)o)de)"
+Output: "lee(t(c)o)de"
+Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+Example 2:
+
+Input: s = "a)b(c)d"
+Output: "ab(c)d"
+Example 3:
+
+Input: s = "))(("
+Output: ""
+Explanation: An empty string is also valid.
+Example 4:
+
+Input: s = "(a(b(c)d)"
+Output: "a(b(c)d)"
+ 
+
+Constraints:
+
+1 <= s.length <= 10^5
+s[i] is one of  '(' , ')' and lowercase English letters.
+*/
+
+
+
+class Solution {
+        
+    fun minRemoveToMakeValid(s: String): String {
+        
+        val stack: Stack<Int> = Stack()
+        
+        val listToRemove = hashSetOf<Int>()
+        
+        for ((i, ch) in s.withIndex()) {
+            
+            when{
+                ch == '(' -> stack.push(i)
+                ch == ')' -> {
+                    if (stack.isEmpty()) {
+                        listToRemove.add(i)
+                    } else {
+                        stack.pop()
+                    }
+                }
+                // else -> continue
+            }
+            
+        }
+        
+        while(stack.isNotEmpty()) {
+            listToRemove.add(stack.pop())
+        }
+        
+        val sb = StringBuilder("")
+        for ((i, ch) in s.withIndex()) {
+            if (listToRemove.contains(i)) continue
+            sb.append(ch)
+        }
+        return sb.toString()
+        
+    }
+}
